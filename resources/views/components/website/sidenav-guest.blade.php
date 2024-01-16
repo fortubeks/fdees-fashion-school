62ec01519654ee99643c9009b1dc801bf9926d5d
Fortune Bekee <fortubeks@gmail.com>
2024-01-16 17:12:29 +0100

    .

---
+++ resources/views/components/website/sidenav-guest.blade.php
@@ -0,0 +1,25 @@
+<nav
+    class="navbar navbar-expand-lg blur border-radius-sm top-0 z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
+    <div class="container-fluid px-1">
+        <a class="navbar-brand font-weight-bolder ms-lg-0 " href="{{url('/')}}">
+            {{env('APP_NAME')}}
+        </a>
+        <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon mt-2">
+                <span class="navbar-toggler-bar bar1"></span>
+                <span class="navbar-toggler-bar bar2"></span>
+                <span class="navbar-toggler-bar bar3"></span>
+            </span>
+        </button>
+        <div class="collapse navbar-collapse" id="navigation">
+            <ul class="navbar-nav mx-auto ms-xl-auto">
+               
+                
+            </ul>
+            <ul class="navbar-nav d-lg-block d-none">
+                
+            </ul>
+        </div>
+    </div>
+</nav>
